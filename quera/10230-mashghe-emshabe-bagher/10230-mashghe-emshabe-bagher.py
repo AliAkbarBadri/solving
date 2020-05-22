0095b1cf9f92d66f0718de9f6b3b406383a93661
aliakbarbadri <aabadri74@gmail.com>
2020-05-23 00:10:18 +0430

    Add 10230-mashghe-emshabe-bagher

---
+++ quera/10230-mashghe-emshabe-bagher/10230-mashghe-emshabe-bagher.py
@@ -0,0 +1,6 @@
+n = input()
+angles = n.split(" ")
+result = "No"
+if int(angles[0]) and int(angles[1]) and int(angles[2]) and int(angles[0])+int(angles[1])+int(angles[2]) == 180:
+    result = "Yes"
+print(result)
