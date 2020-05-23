bd7cc7fc0dcaf4a43c3b87f642091e2b629b9188
aliakbarbadri <aabadri74@gmail.com>
2020-05-24 03:12:02 +0430

    Add 10162-rooze-azadi-bayan-dar-barare

---
+++ quera/10162-rooze-azadi-bayan-dar-barare/10162-rooze-azadi-bayan-dar-barare.py
@@ -0,0 +1,10 @@
+# https://quera.ir/problemset/contest/10162/سؤال-روز-آزادی-بیان-در-برره
+
+n = int(input())
+
+if n % 2 == 0:
+    angry = "Bala Barare"
+else:
+    angry = "Payin Barare"
+
+print(angry)
