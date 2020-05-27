8c243f965740155534f97bf3971c482bb7bb426b
aliakbarbadri <aabadri74@gmail.com>
2020-05-27 19:10:56 +0430

    Add 28947-m-sin

---
+++ quera/28947-m-sin/28947-m-sin.py
@@ -0,0 +1,8 @@
+# https://quera.ir/problemset/contest/28947/سؤال-پیاده-سازی-امسین
+
+m = int(input())
+
+sins = ["sib", "sir", "somagh", "samanoo", "senjed", "serke", "sabzi"]
+
+for i in range(m):
+    print(sins[i])
