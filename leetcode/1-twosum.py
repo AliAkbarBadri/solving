c77283423b891fe47709ea537b7ef81c47034254
AliAkbarBadri <aliakbarbadr@gmail.com>
2023-07-07 13:44:48 +0330

    add twosum two pass hashmap

--- leetcode/1-twosum.py
+++ leetcode/1-twosum.py
@@ -6,10 +6,13 @@ from typing import List
 
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
-        for i in range(len(nums)):
-            for j in range(i+1, len(nums)):
-                if nums[i]+nums[j] == target:
-                    return [i, j]
+        hashmap = {}
+        for idx, value in enumerate(nums):
+            hashmap[value] = idx
+        for idx, value in enumerate(nums):
+            idx2 = hashmap.get(target - value)
+            if idx2 and idx2 != idx:
+                return [idx, idx2]
 
 
 sol = Solution()
